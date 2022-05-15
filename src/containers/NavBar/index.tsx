62ced2e95e22c510ab4e9485a65975056fde6b59
patrykkubala <patrykkubala1999@gmail.com>
2022-05-16 00:29:36 +0200

    added the navbar component

---
+++ src/containers/NavBar/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles.css";
+
+function NavBar() {
+  return <div className="navbar">index</div>;
+}
+
+export default NavBar;
---
+++ src/pages/Main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "../containers/NavBar";
+
+function Main() {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+}
+
+export default Main;
