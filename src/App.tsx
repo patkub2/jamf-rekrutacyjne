e492f8cce3840b5daa5b7e9cab247c282b112f48
patrykkubala <patrykkubala1999@gmail.com>
2022-05-16 20:49:11 +0200

    installed redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from "react";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import usersReducer from "./features/Users";
+
 import Main from "./pages/Main";
 
+const store = configureStore({
+  reducer: {
+    users: usersReducer,
+  },
+});
 function App() {
   return (
-    <div className="App">
-      <Main />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Main />
+      </div>
+    </Provider>
   );
 }
 
---
+++ src/data/Data.tsx
@@ -0,0 +1,27 @@
+// eslint-disable-next-line import/prefer-default-export
+export const UsersData = [
+  {
+    id: 1,
+    name: "Apple watch",
+    price: "1999,00 zł",
+    img: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MKU93_VW_34FR+watch-40-alum-gold-nc-se_VW_34FR_WF_CO?wid=1400&hei=1400&trim=1,0&fmt=p-jpg&qlt=95&.v=1632171039000,1630712417000",
+  },
+  {
+    id: 2,
+    name: "MacBook Air",
+    price: "4950,00 zł",
+    img: "https://a.allegroimg.com/original/11cef1/1c45e3f346f99a6ec29d6d8b625a/Apple-MacBook-Air-M1-8GB-256-Mac-OS",
+  },
+  {
+    id: 3,
+    name: "iPad",
+    price: "2395,00 zł",
+    img: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/ipad-pro-12-select-wifi-spacegray-202104_FMT_WHH?wid=1945&hei=2000&fmt=jpeg&qlt=95&.v=1617126635000",
+  },
+  {
+    id: 4,
+    name: "iMac Pro",
+    price: "9700,00 zł",
+    img: "https://static1.ultraupgrade.pl/pol_pl_Potezny-Apple-iMac-PRO-5K-Xeon-10-Core-3GHz-VEGA-16GB-128GB-RAM-1TB-SSD-101221_2.jpg",
+  },
+];
