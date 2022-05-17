263a365fb90ab8f89aaf560b60f3f185d8044d1c
patrykkubala <patrykkubala1999@gmail.com>
2022-05-17 13:21:37 +0200

    added burger menu

--- src/containers/NavBar/index.tsx
+++ src/containers/NavBar/index.tsx
@@ -1,8 +1,25 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from "react";
+
 import "./styles.css";
 
-function NavBar() {
-  return <div className="navbar">index</div>;
+function Navbar() {
+  return (
+    <div className="hamburger-menu">
+      <input id="menu__toggle" type="checkbox" />
+      <label className="menu__btn" htmlFor="menu__toggle">
+        <span />
+      </label>
+
+      <ul className="menu__box">
+        <li>Home</li>
+        <li>About</li>
+        <li>Team</li>
+        <li>Contact</li>
+        <li>Twitter</li>
+      </ul>
+    </div>
+  );
 }
 
-export default NavBar;
+export default Navbar;
